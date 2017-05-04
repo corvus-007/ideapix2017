d01906eec1182af07c366ce9b8f8c2b6f72381cd
Denis <corvus007@yandex.ru>
2017-05-04 16:33:01 +0300

    Создает страницу проета, второстепенную страницу

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  /*==================================
+  =            Toggle nav            =
+  ==================================*/
+
   var navToggler = document.querySelector('.nav-toggler');
   var siteCover = document.querySelector('.site-cover');
+  var navTogglerIconItems = navToggler.querySelectorAll('.nav-toggler__icon');
 
   function toggleSiteCover() {
     siteCover.classList.toggle('site-cover--opened');
+    Array.prototype.forEach.call(navTogglerIconItems, function(item) {
+      item.classList.toggle('nav-toggler__icon--hidden');
+    });
   }
 
   if (navToggler) {
@@ -13,32 +22,38 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
+  /*=====  End of Toggle nav  ======*/
+
+
 
   /*=====================================
   =            Hello reviews            =
   =====================================*/
 
   var helloReviews = document.querySelector('.hello-reviews');
-  var helloReviewsItems = helloReviews.querySelectorAll('.hello-reviews__item');
-  var helloReviewsItemsCount = (helloReviewsItems.length - 1);
 
-  $(helloReviews).on('click', '.hello-reviews__reload', function() {
-    var $self = $(this);
-    var $helloReview = $self.closest('.hello-reviews__item');
-    var helloReviewIndex = $helloReview.index();
+  if (helloReviews) {
+    var helloReviewsItems = helloReviews.querySelectorAll('.hello-reviews__item');
+    var helloReviewsItemsCount = (helloReviewsItems.length - 1);
 
-    if (helloReviewIndex < helloReviewsItemsCount) {
-      helloReviewIndex++;
-    } else {
-      helloReviewIndex = 0;
-    }
+    $(helloReviews).on('click', '.hello-reviews__reload', function() {
+      var $self = $(this);
+      var $helloReview = $self.closest('.hello-reviews__item');
+      var helloReviewIndex = $helloReview.index();
 
-    $(helloReviewsItems).removeClass('hello-reviews__item--active');
-    helloReviewsItems[helloReviewIndex].classList.add('hello-reviews__item--active');
-  });
+      if (helloReviewIndex < helloReviewsItemsCount) {
+        helloReviewIndex++;
+      } else {
+        helloReviewIndex = 0;
+      }
+
+      $(helloReviewsItems).removeClass('hello-reviews__item--active');
+      helloReviewsItems[helloReviewIndex].classList.add('hello-reviews__item--active');
+    });
+  }
+
 
   /*=====  End of Hello reviews  ======*/
-
 
 
 
